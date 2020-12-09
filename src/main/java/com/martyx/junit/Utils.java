198d496c406b455eed0ea782ea8cd6d1f3e13744
prometheus <martin1.bugar@gmail.com>
2020-12-09 15:41:44 +0100

    viacTestovNaraz

--- src/main/java/com/martyx/junit/Utils.java
+++ src/main/java/com/martyx/junit/Utils.java
@@ -1,8 +1,17 @@
 package com.martyx.junit;
 
+import java.math.BigDecimal;
+
 public class Utils {
 
     public int sum(int a, int b){
         return a + b;
+    }
+
+    private final int aktualnaDan = 20;
+
+    public BigDecimal pripocitajDan(BigDecimal suma){
+        return suma.add(suma.divide(BigDecimal.valueOf(100)).
+                multiply(BigDecimal.valueOf(aktualnaDan)));
     }
 }
--- src/test/java/com/martyx/junit/UtilsTest.java
+++ src/test/java/com/martyx/junit/UtilsTest.java
@@ -2,13 +2,15 @@ package com.martyx.junit;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
 
     @Test
     void failedtest(){
-            fail(); //metoda na zlyhanie testu
+          //   fail(); //metoda na zlyhanie testu
     }
 
     @Test
@@ -22,6 +24,26 @@ class UtilsTest {
         int expectedValue = 2;
         int actualValue = utils.sum(1,1);
         assertEquals( expectedValue, actualValue); // porovnava dve hodnity ci sa rovnaju
+    }
+
+    @Test
+    void suma100PlusDanJe120(){
+        Utils utils = new Utils();
+        assertEquals(BigDecimal.valueOf(120L), utils.pripocitajDan(BigDecimal.valueOf(100L)) );
+    }
+
+    @Test
+    public void suma141PlusDanJe169Cela2(){
+        Utils utils = new Utils();
+        assertEquals(BigDecimal.valueOf(169.20).setScale(2),
+                utils.pripocitajDan(BigDecimal.valueOf(141L)));
+    }
+
+    @Test
+    public void suma10PlusDanNieJe100(){
+        Utils utils = new Utils();
+        assertNotEquals(BigDecimal.valueOf(100).setScale(2),
+        utils.pripocitajDan(BigDecimal.valueOf(10).setScale(2)), "Suma 10 + Dan nie je 100");
     }
 
 }
